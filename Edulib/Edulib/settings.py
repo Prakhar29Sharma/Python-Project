77a6bc2f36b24888c476a65113d54bfa04e628ca
Dhaneshwari Tendle <110600266+dhaneshwaritendle@users.noreply.github.com>
2023-04-04 21:40:08 +0530

    n o changes

--- Edulib/Edulib/settings.py
+++ Edulib/Edulib/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "my_database",
         "USER": "root",
-        "PASSWORD": "prakhar@29",
+        "PASSWORD": "11May1996",
         "HOST": "127.0.0.1",
-        "PORT": "3306",
+        "PORT": "3310",
         "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
